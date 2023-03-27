d7ad091e9429724537adeaecd12bf03bc35aa32b
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-03-27 20:47:07 +1100

    setting up the stores

--- src/store.js
+++ src/store.js
@@ -1,5 +1,16 @@
 import { getStorageItem, setStorageItem } from "./utils.js";
 let store = [];
-const setupStore = () => {};
+const setupStore = (products) => {
+  store = products.map((product) => {
+    // console.log(product);
+    const {
+      id,
+      fields: { featured, name, price, company, colors, image: img },
+    } = product;
+    const image = img[0].thumbnails.large.url;
+    return { id, featured, name, company, price, colors, image };
+  });
+};
+console.log(store);
 const findProduct = () => {};
 export { store, setupStore, findProduct };
