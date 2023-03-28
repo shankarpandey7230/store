a2eba9e0be9f2220904cb46c9117eda161152887
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-03-29 09:45:35 +1100

    working on filtering from search input

--- src/filters/search.js
+++ src/filters/search.js
@@ -1,5 +1,31 @@
 import { getElement } from "../utils.js";
 import display from "../displayProducts.js";
-const setupSearch = () => {};
+const setupSearch = (store) => {
+  const form = getElement(".input-form");
+  const nameInput = getElement(".search-input");
+  form.addEventListener("keyup", function () {
+    const value = nameInput.value;
+    // console.log(value);
+    if (value) {
+      const newStore = store.filter((product) => {
+        let { name } = product;
+        // console.log(name);
+        name = name.toLowerCase();
+        if (name.startsWith(value)) {
+          return product;
+        }
+      });
+      display(newStore, getElement(".products-container"));
+      if (newStore.length < 1) {
+        const products = getElement(".products-container");
+        products.innerHTML = `<h3 class="filter-error">
+        Sorry, no products matched your search</h3>`;
+      }
+      //   console.log(newStore);
+    } else {
+      display(store, getElement(".products-container"));
+    }
+  });
+};
 
 export default setupSearch;
