9ba9d7aab80ee60e34b0d85d846c06a00ecf2756
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-04-01 23:36:24 +1100

    removing item from the cart

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -64,6 +64,9 @@ function displaycartItemsDOM() {
     addToCartDOM(cartItem);
   });
 }
+function removeItem(id) {
+  cart = cart.filter((cartItem) => cartItem.id != id);
+}
 function increaseAmount(id) {
   let newAmount;
   cart = cart.map((cartItem) => {
@@ -75,7 +78,24 @@ function increaseAmount(id) {
   });
   return newAmount;
 }
-function setupCartFunctionality() {}
+function setupCartFunctionality() {
+  cartItemsDOM.addEventListener("click", function (e) {
+    const element = e.target;
+    const parent = e.target.parentElement;
+    const id = e.target.dataset.id;
+    const parentID = e.target.parentElement.dataset.id;
+    // remove
+    if (element.classList.contains("cart-item-remove-btn")) {
+      removeItem(id);
+      parent.parentElement.remove();
+    }
+    // increase
+    // decrease
+    displayCartItemCount();
+    displayCartTotal();
+    setStorageItem("cart", cart);
+  });
+}
 const init = () => {
   // console.log(cart);
   // display amount of cart items
